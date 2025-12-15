23f498f64363d9d3b43cea339c0438b815a6b39a
ArtemZayarny <artyom.zayarny@gmail.com>
2025-12-15 18:28:16 +0200

    Set up nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto py-8 px-4">
+      <h1 className="text-3xl font-bold mb-4">Task Tracker</h1>
+      <p>Task Tracker application will be built here.</p>
+    </main>
+  )
+}
+
